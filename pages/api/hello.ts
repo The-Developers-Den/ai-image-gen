cf5c26e95e4ad7ece735e1b79a1ae46afb4aca35
Pratham <prathamsinghal2003@gmail.com>
2023-03-31 00:40:38 +0530

    feat: add fetching api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type { NextApiRequest, NextApiResponse } from "next";
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const response = await openai.createImage({
+    prompt: "A dog catching frisbee",
+    n: 1,
+    size: "1024x1024",
+  });
+  const image_url = response.data.data[0].url;
+  res.status(200).json({ name: image_url });
 }
