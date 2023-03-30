33fcf275473aa89794b5d722ebef22dfcc4d6caa
Pratham <prathamsinghal2003@gmail.com>
2023-03-31 02:59:03 +0530

    feat: add frontend code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link href="/">
-        <h2>WALL-E</h2>
+        <Image
+          src="/next.svg"
+          alt="logo"
+          className="w-28 object-contain"
+          width={100}
+          height={100}
+        />
         {/* <img src={logo} alt="logo" className="w-28 object-contain" /> */}
       </Link>
 
